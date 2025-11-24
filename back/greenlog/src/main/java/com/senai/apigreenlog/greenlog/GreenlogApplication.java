81e858ab278c6b823d209d69a2f119e1507c6f92
GabrielBragaOliveira <ninjagamer9795286@gmail.com>
2025-11-24 20:40:16 -0300

    V1.0.0

---
+++ back/greenlog/src/main/java/com/senai/apigreenlog/greenlog/GreenlogApplication.java
@@ -0,0 +1,13 @@
+package com.senai.apigreenlog.greenlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenlogApplication.class, args);
+	}
+
+}
