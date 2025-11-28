dd4bd2a7de6c416b2453c524d6d73af2060069bf
kayque <kayquefreitas08@gmail.com>
2025-11-28 12:11:34 -0300

    1.2.0 Implementei a tela de caminhao, corrigir o CanActive e o CanDeActive e a rota que levava sempre para o null

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { MessageService } from 'primeng/api';
+import { MessageService, ConfirmationService } from 'primeng/api';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimations(),
-    MessageService 
+    MessageService,
+    ConfirmationService
   ]
 };
