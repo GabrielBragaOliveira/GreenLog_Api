dfcdf539fbe8f827ba89943a0f3201808c6d7172
LuizBastos98 <tilionbr@gmail.com>
2025-11-29 19:38:53 -0300

    V1.7.0 Criado a tela de gerenciamento de usuários com o CRUD.

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -12,80 +12,95 @@ import { ConexoesFormComponent } from './funcionalidades/conexoes/conexoes-form/
 import { RoteamentoCalculoComponent } from './funcionalidades/rotas/roteamento-calculo/roteamento-calculo.component';
 import { BairrosListaComponent } from './funcionalidades/bairros/bairros-lista/bairros-lista.component';
 import { BairrosFormComponent } from './funcionalidades/bairros/bairros-form/bairros-form.component';
+import { UsuariosListaComponent } from './funcionalidades/usuarios/usuarios-lista/usuarios-lista.component';
+import { UsuariosFormComponent } from './funcionalidades/usuarios/usuarios-form/usuarios-form.component';
 
-export const routes: Routes = [{
-  path: 'login',
-  component: LoginComponent
-},
-
-{
-  path: '',
-  component: MainLayoutComponent,
-  canActivate: [authGuard],
-  children: [
-    { path: '', redirectTo: 'rotas', pathMatch: 'full' },
-    {
-      path: 'rotas',
-      component: RoteamentoCalculoComponent
-    },
-    {
-      path: 'caminhoes',
-      component: CaminhaoListaComponent
-    },
-    {
-      path: 'caminhoes/novo',
-      component: CaminhaoFormComponent,
-      canDeactivate: [formExitGuard]
-    },
-    {
-      path: 'caminhoes/editar/:id',
-      component: CaminhaoFormComponent,
-      canDeactivate: [formExitGuard]
-    },
-    {
-      path: 'pontos-coleta',
-      component: PontosListaComponent
-    },
-    {
-      path: 'pontos-coleta/novo',
-      component: PontosFormComponent,
-      canDeactivate: [formExitGuard]
-    },
-    {
-      path: 'pontos-coleta/editar/:id',
-      component: PontosFormComponent,
-      canDeactivate: [formExitGuard]
-    },
-    {
-      path: 'conexoes',
-      component: ConexoesListaComponent
-    },
-    {
-      path: 'conexoes/novo',
-      component: ConexoesFormComponent,
-      canDeactivate: [formExitGuard]
-    },
-    {
-      path: 'conexoes/editar/:id',
-      component: ConexoesFormComponent,
-      canDeactivate: [formExitGuard]
-    },
-    {
-      path: 'bairros',
-      component: BairrosListaComponent
-    },
-    {
-      path: 'bairros/novo',
-      component: BairrosFormComponent,
-      canDeactivate: [formExitGuard]
-    },
-    {
-      path: 'bairros/editar/:id',
-      component: BairrosFormComponent,
-      canDeactivate: [formExitGuard]
-    }
-  ]
-},
-
-{ path: '**', redirectTo: '' }
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: '', redirectTo: 'rotas', pathMatch: 'full' },
+      {
+        path: 'rotas',
+        component: RoteamentoCalculoComponent
+      },
+      {
+        path: 'caminhoes',
+        component: CaminhaoListaComponent
+      },
+      {
+        path: 'caminhoes/novo',
+        component: CaminhaoFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'caminhoes/editar/:id',
+        component: CaminhaoFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'pontos-coleta',
+        component: PontosListaComponent
+      },
+      {
+        path: 'pontos-coleta/novo',
+        component: PontosFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'pontos-coleta/editar/:id',
+        component: PontosFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'conexoes',
+        component: ConexoesListaComponent
+      },
+      {
+        path: 'conexoes/novo',
+        component: ConexoesFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'conexoes/editar/:id',
+        component: ConexoesFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'bairros',
+        component: BairrosListaComponent
+      },
+      {
+        path: 'bairros/novo',
+        component: BairrosFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'bairros/editar/:id',
+        component: BairrosFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'usuarios',
+        component: UsuariosListaComponent
+      },
+      {
+        path: 'usuarios/novo',
+        component: UsuariosFormComponent,
+        canDeactivate: [formExitGuard]
+      },
+      {
+        path: 'usuarios/editar/:id',
+        component: UsuariosFormComponent,
+        canDeactivate: [formExitGuard]
+      }
+    ]
+  },
+  { path: '**', redirectTo: '' }
 ];
