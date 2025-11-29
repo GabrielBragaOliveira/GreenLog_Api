5c656371b54481ab38d32db86a30abe90b01b65e
kayque <kayquefreitas08@gmail.com>
2025-11-29 16:09:20 -0300

    Tela de Calcular a rota e armazenar se necessario

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -4,71 +4,72 @@ import { MainLayoutComponent } from './estrutura/main-layout/main-layout.compone
 import { authGuard } from './nucleo/guards/auth.guard';
 import { formExitGuard } from './nucleo/guards/form-exit.guard';
 import { CaminhaoListaComponent } from './funcionalidades/caminhao/caminhao-lista/caminhao-lista.component';
-import { CaminhaoFormComponent } from './funcionalidades/caminhao/caminhao-form/caminhao-form.component';
+import { CaminhaoFormComponent } from './funcionalidades/caminhao/caminhao-form/caminhao-form.component'
 import { PontosListaComponent } from './funcionalidades/pontos/pontos-lista/pontos-lista.component';
 import { PontosFormComponent } from './funcionalidades/pontos/pontos-form/pontos-form.component';
 import { ConexoesListaComponent } from './funcionalidades/conexoes/conexoes-lista/conexoes-lista.component';
 import { ConexoesFormComponent } from './funcionalidades/conexoes/conexoes-form/conexoes-form.component';
+import { RoteamentoCalculoComponent } from './funcionalidades/rotas/roteamento-calculo/roteamento-calculo.component';
 
-export const routes: Routes = [
-  { 
-    path: 'login', 
-    component: LoginComponent 
-  },
-  
-  {
-    path: '',
-    component: MainLayoutComponent,
-    canActivate: [authGuard],
-    children: [
-      { path: '', redirectTo: 'caminhoes', pathMatch: 'full' },
-      
-      { 
-        path: 'caminhoes', 
-        component: CaminhaoListaComponent 
-      },
-      { 
-        path: 'caminhoes/novo', 
-        component: CaminhaoFormComponent,
-        canDeactivate: [formExitGuard]
-      },
-      { 
-        path: 'caminhoes/editar/:id', 
-        component: CaminhaoFormComponent,
-        canDeactivate: [formExitGuard]
-      },
+export const routes: Routes = [{
+  path: 'login',
+  component: LoginComponent
+},
 
-      { 
-        path: 'pontos-coleta', 
-        component: PontosListaComponent 
-      },
-      { 
-        path: 'pontos-coleta/novo', 
-        component: PontosFormComponent,
-        canDeactivate: [formExitGuard]
-      },
-      { 
-        path: 'pontos-coleta/editar/:id', 
-        component: PontosFormComponent,
-        canDeactivate: [formExitGuard]
-      },
+{
+  path: '',
+  component: MainLayoutComponent,
+  canActivate: [authGuard],
+  children: [
+    { path: '', redirectTo: 'rotas', pathMatch: 'full' },
+    {
+      path: 'rotas',
+      component: RoteamentoCalculoComponent
+    },
+    {
+      path: 'caminhoes',
+      component: CaminhaoListaComponent
+    },
+    {
+      path: 'caminhoes/novo',
+      component: CaminhaoFormComponent,
+      canDeactivate: [formExitGuard]
+    },
+    {
+      path: 'caminhoes/editar/:id',
+      component: CaminhaoFormComponent,
+      canDeactivate: [formExitGuard]
+    },
+    {
+      path: 'pontos-coleta',
+      component: PontosListaComponent
+    },
+    {
+      path: 'pontos-coleta/novo',
+      component: PontosFormComponent,
+      canDeactivate: [formExitGuard]
+    },
+    {
+      path: 'pontos-coleta/editar/:id',
+      component: PontosFormComponent,
+      canDeactivate: [formExitGuard]
+    },
+    {
+      path: 'conexoes',
+      component: ConexoesListaComponent
+    },
+    {
+      path: 'conexoes/novo',
+      component: ConexoesFormComponent,
+      canDeactivate: [formExitGuard]
+    },
+    {
+      path: 'conexoes/editar/:id',
+      component: ConexoesFormComponent,
+      canDeactivate: [formExitGuard]
+    }
+  ]
+},
 
-      { 
-        path: 'conexoes', 
-        component: ConexoesListaComponent 
-      },
-      { 
-        path: 'conexoes/novo', 
-        component: ConexoesFormComponent,
-        canDeactivate: [formExitGuard]
-      },
-      { 
-        path: 'conexoes/editar/:id', 
-        component: ConexoesFormComponent,
-        canDeactivate: [formExitGuard]
-      }
-    ]
-  },
-  
-  { path: '**', redirectTo: '' }
+{ path: '**', redirectTo: '' }
 ];
