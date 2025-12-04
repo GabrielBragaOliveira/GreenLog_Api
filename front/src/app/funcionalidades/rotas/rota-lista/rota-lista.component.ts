62a31e411515a29816f6b4b62130c9c701393b2e
kayque <kayquefreitas08@gmail.com>
2025-12-04 10:39:26 -0300

    V1.12.0 Tela de visualizacao e exclusao de rotas feitas

---
+++ front/src/app/funcionalidades/rotas/rota-lista/rota-lista.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { DialogModule } from 'primeng/dialog';
+import { TagModule } from 'primeng/tag';
+import { ListboxModule } from 'primeng/listbox';
+import { RotaService } from '../../../nucleo/servicos/rota.service';
+import { RotaResponse } from '../../../compartilhado/models/rota.model';
+
+@Component({
+  selector: 'app-rota-lista',
+  standalone: true,
+  imports: [
+    CommonModule,
+    TableModule,
+    ButtonModule,
+    TooltipModule,
+    DialogModule,
+    TagModule,
+    ListboxModule
+  ],
+  templateUrl: './rota-lista.component.html',
+  styles: [`
+    :host ::ng-deep .p-dialog-content {
+      padding: 0 1.5rem 1.5rem 1.5rem;
+    }
+  `]
+})
+export class RotaListaComponent implements OnInit {
+  private rotaService = inject(RotaService);
+  private confirmationService = inject(ConfirmationService);
+  private messageService = inject(MessageService);
+  rotas = signal<RotaResponse[]>([]);
+  loading = signal<boolean>(false);
+  exibirDetalhes = signal<boolean>(false);
+  rotaSelecionada = signal<RotaResponse | null>(null);
+
+  ngOnInit() {
+    this.carregarRotas();
+  }
+
+  carregarRotas() {
+    this.loading.set(true);
+    this.rotaService.listar().subscribe({
+      next: (dados) => {
+        this.rotas.set(dados);
+        this.loading.set(false);
+      },
+      error: (erro) => {
+        this.loading.set(false);
+      }
+    });
+  }
+
+  verDetalhes(rota: RotaResponse) {
+    this.rotaSelecionada.set(rota);
+    this.exibirDetalhes.set(true);
+  }
+
+  confirmarExclusao(rota: RotaResponse) {
+    this.confirmationService.confirm({
+      message: `Tem certeza que deseja excluir a rota <b>${rota.nome}</b>?`,
+      header: 'Confirmação de Exclusão',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sim',
+      rejectLabel: 'Não',
+      acceptButtonStyleClass: 'p-button-danger p-button-text',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: () => {
+        this.excluir(rota.id);
+      }
+    });
+  }
+
+  private excluir(id: number) {
+    this.loading.set(true);
+    this.rotaService.excluir(id).subscribe({
+      next: () => {
+        this.carregarRotas(); 
+      },
+      error: () => {
+        this.loading.set(false);
+      }
+    });
+  }
+}
